63f7cf6b45556f0453bc5da66ee66b817d4d1c5c
thisisbrix901 <bmadeira7@gmail.com>
2018-12-04 20:30:12 -0500

    chat done

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Chatkit = require("@pusher/chatkit-server");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+const chatkit = new Chatkit.default({
+  instanceLocator: "v1:us1:30716a11-5345-4a7c-acb8-9e9eefa5092c",
+  key:
+    "1e2b061b-9815-4890-8943-21429ed62352:+q7Ge2dB5z+2/yccR/IkYoNHdOKainD3pBn2WNCO3UM="
+});
 
-const PORT = 3001
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/users", (req, res) => {
+  const { username } = req.body;
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === "services/chatkit/user_already_exists") {
+        res.sendStatus(200);
+      } else {
+        res.status(error.status).json(error);
+      }
+    });
+});
+
+app.post("/authenticate", (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id });
+  res.status(authData.status).send(authData.body);
+});
+
+const PORT = 3001;
 app.listen(PORT, err => {
   if (err) {
-    console.error(err)
+    console.error(err);
   } else {
-    console.log(`Running on port ${PORT}`)
+    console.log(`Running on port ${PORT}`);
   }
-})
+});
